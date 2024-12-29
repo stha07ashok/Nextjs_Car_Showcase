18829a473229c8f19e78ae3cc3729c6d47cedfd4
Name <user@example.com>
2024-12-30 00:24:50 +0545

    carshowcase

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="overflow-hidden">
+        <Hero />
+      </main>
+    </div>
+  );
+}
